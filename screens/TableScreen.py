7de99670fe5aa34604658523004136266a36bce6
JoaoPedroMDP <joao.pedro.mdp@outlook.com>
2023-11-07 16:24:34 -0300

    [change] Agora as tabelas possuem título [fix] Era possível criar uma task sem Texto

--- screens/TableScreen.py
+++ screens/TableScreen.py
@@ -1,5 +1,5 @@
 #  coding: utf-8
-from PyQt5.QtWidgets import QMainWindow, QListWidget, QListWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QListWidget, QListWidgetItem, QVBoxLayout, QHBoxLayout, QLabel, QFrame
 from PyQt5.uic import loadUi
 
 from classes.Column import Column
@@ -19,6 +19,7 @@ class TableScreen(QMainWindow, Ui_TableScreen):
         self.tables = self.get_tables()
         self.opened_table = self.tables[0]
         self.table_name.setText(self.opened_table.name)
+        self.table_layout: QHBoxLayout = QHBoxLayout(self.table)
         self.update_table()
 
     def get_tables(self):
@@ -27,17 +28,42 @@ class TableScreen(QMainWindow, Ui_TableScreen):
 
     def create_task(self):
         task_title = self.create_task_text_input.text()
+        if task_title == "":
+            return
+
         table_first_column: Column = self.opened_table.columns[0]
         task: Task = Task(task_title, table_first_column.id)
         table_first_column.add_task(task)
         self.update_table()
 
     def update_table(self):
-        print(self.opened_table)
+        self.clear_table()
+
         for column in self.opened_table.columns:
-            new_column = QListWidget(self.table)
-            for task in column.tasks:
-                new_item = QListWidgetItem(task.name, new_column)
-                new_column.addItem(new_item)
+            print("Adicionando coluna" + column.name)
+            new_column = self.create_column(column)
+            self.table_layout.addWidget(new_column)
 
         self.table.update()
+
+    def clear_table(self):
+        while not self.table_layout.isEmpty():
+            to_delete = self.table_layout.takeAt(0)
+            if to_delete is not None:
+                widget = to_delete.widget()
+                widget.deleteLater()
+
+    def create_column(self, column: Column):
+        column_frame = QFrame(self.table)
+        column_layout = QVBoxLayout(column_frame)
+
+        new_column = QListWidget(self.table)
+        for task in column.tasks:
+            print("Adicionando task" + task.name)
+            new_item = QListWidgetItem(task.name, new_column)
+            new_column.addItem(new_item)
+
+        column_header = QLabel(column.name, new_column)
+        column_layout.addWidget(column_header)
+        column_layout.addWidget(new_column)
+        return column_frame
